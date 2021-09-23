9e892bd5852ea54707cfde8921d162e600f88589
Jaeyoung Park <jaeyoungs.park@gmail.com>
2021-09-23 18:58:42 +0900

    [temp] refactoring - connector & server

--- network/server.hpp
+++ network/server.hpp
@@ -10,7 +10,7 @@
 
 namespace gnf {
 
-template <typename Socket, typename MessageType> class Connections {
+template <typename Socket, typename MessageType> class Connector {
 
   using ChannelType = Channel<Socket, MessageType>;
   using ChannelCreatedHandlerType =
@@ -21,9 +21,6 @@ template <typename Socket, typename MessageType> class Connections {
   ChannelCreatedHandlerType _onChannelCreated;
 
   auto startAccepting() -> void {
-
-		std::cout << "start accepting clients\n";
-
     auto socket = std::make_unique<Socket>(_asioContext);
     // unique_ptr will be moved to labmda. So we keep it as ref for a moment.
     auto &ref = *socket;
@@ -37,10 +34,11 @@ template <typename Socket, typename MessageType> class Connections {
   }
 
 public:
-  Connections(boost::asio::io_context &context)
-      : _asioContext(context), _asioAcceptor(context) {}
+  Connector(boost::asio::io_context &context, ChannelCreatedHandlerType handler)
+      : _asioContext(context), _asioAcceptor(context),
+	_onChannelCreated(handler) {}
 
-  auto start(int port, ChannelCreatedHandlerType handler) {
+  auto start(int port) {
 
     boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), port);
 
@@ -50,9 +48,6 @@ public:
     _asioAcceptor.bind(endpoint);
     _asioAcceptor.listen();
 
-    _onChannelCreated = handler;
-
-
     startAccepting();
   }
 
@@ -61,7 +56,7 @@ public:
     _onChannelCreated = nullptr;
   }
 
-  ~Connections() { stop(); }
+  ~Connector() { stop(); }
 };
 
 /*
@@ -71,26 +66,26 @@ public:
  * */
 template <typename Socket, typename MessageType> class GenericServer {
 private:
-  // threads
-  int _threadCount;
-  std::vector<std::thread> _threadPool;
-
-  // asio for a server
   boost::asio::io_service _asioContext;
 
-  // Connections
+  std::vector<std::thread> _workers;
+
+  // Connector
+  Connector<Socket, MessageType> _connector;
+
+  // Sessions
   int _id;
-  Connections<Socket, MessageType> connections;
   std::unordered_map<uint32_t, std::unique_ptr<Channel<Socket, MessageType>>>
       sessions;
 
   auto onConnected(std::unique_ptr<Channel<Socket, MessageType>> channel) {
     auto id = _id++;
-		std::cout << fmt::format("channel created {}\n", id);
     sessions[id] = std::move(channel);
     sessions[id]->registerOnMessageRecieved(
 	[=](auto const &msg) { onMessageRecieved(id, msg); });
     sessions[id]->start();
+
+    onSessionCreated(id);
   }
 
   virtual auto onSessionCreated(int id) -> void {}
@@ -99,23 +94,25 @@ private:
       -> void {}
 
 public:
-  GenericServer(uint32_t port, int threadCount)
-      : connections(_asioContext), _threadCount(threadCount) {
+  GenericServer()
+      : _id(0), _connector(_asioContext, [=](auto channel) {
+	  onConnected(std::move(channel));
+	}) {}
 
-    connections.start(port,
-		      [=](auto channel) { onConnected(std::move(channel)); });
+  auto start(uint32_t port, int threadCount) {
 
-    // start pool of threads to process the asio events
-    for (int i = 0; i < _threadCount; ++i) {
-      _threadPool.emplace_back([=] { _asioContext.run(); });
+    _connector.start(port);
+
+    for (int i = 0; i < threadCount; ++i) {
+      _workers.emplace_back([=] { _asioContext.run(); });
     }
   }
 
   virtual ~GenericServer() {
-    for (auto &t : _threadPool) {
-      if (!t.joinable())
+    for (auto &worker : _workers) {
+      if (!worker.joinable())
 	continue;
-      t.join();
+      worker.join();
     }
   }
 };
